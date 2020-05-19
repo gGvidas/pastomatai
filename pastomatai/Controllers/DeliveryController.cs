1305a4fc81db7e4fc153100450361cdd7a30e446
Laura Balciunaite <laura.balciunaite@ktu.edu>
2020-05-19 11:41:34 +0300

    pin confirmation

--- pastomatai/Controllers/DeliveryController.cs
+++ pastomatai/Controllers/DeliveryController.cs
@@ -77,11 +77,13 @@ namespace pastomatai.Controllers
                     { package.PackageState = "EnRoute"; }
                     else if (package.PackageState.Contains("InTerminal"))
                     { package.PackageState = "EnRoute"; }
-                    //else if (package.PackageState.Contains("WaitsForPickup"))
-                    //{ package.PackageState = "Delivered"; }
                     else if (package.PackageState.Contains("EnRoute") && package.FkTerminalidTerminal.HasValue)
-                    { package.PackageState = "WaitsForPickup"; }
-                    else if (package.PackageState.Contains("EnRoute") && !(package.FkTerminalidTerminal.HasValue))
+                    { 
+                        package.PackageState = "WaitsForPickup";
+                        // TODO: package is put into post machine box with id = 666 <-- change it later
+                        package.PostMachineBox = _context.PostMachineBox.Find(666);
+                    }
+                    else if (package.PackageState.Contains("EnRoute") && !package.FkTerminalidTerminal.HasValue)
                     { package.PackageState = "InTerminal"; }
 
                     //Paprastam useriui
@@ -126,14 +128,23 @@ namespace pastomatai.Controllers
 
             foreach (var package in packages)
             {
+                // TODO for testing
+                var currentTerminal = 20;
+
                 if (package.PackageState.Contains("WaitsForCourier"))
                 { package.PackageState = "EnRoute"; }
                 else if (package.PackageState.Contains("InTerminal"))
                 { package.PackageState = "EnRoute"; }
-                //else if (package.PackageState.Contains("WaitsForPickup"))
-                //{ package.PackageState = "Delivered"; }
                 else if (package.PackageState.Contains("EnRoute") && package.FkTerminalidTerminal.HasValue)
-                { package.PackageState = "WaitsForPickup"; }
+                { 
+                    package.PackageState = "WaitsForPickup";
+                    package.PostMachineBox = _context.PostMachineBox.Find(getEmptyByTerminal(currentTerminal));
+                    if (package.PostMachineBox == null)
+                    {
+                        // post machine is full 
+                        return View(packages);
+                    }
+                }
                 else if (package.PackageState.Contains("EnRoute") && !(package.FkTerminalidTerminal.HasValue))
                 { package.PackageState = "InTerminal"; }
 
@@ -150,6 +161,22 @@ namespace pastomatai.Controllers
             }
              _context.SaveChangesAsync();
             return View(packages);
+        }
+
+        private PostMachineBox getEmptyByTerminal(int id)
+        {
+            var postmb = _context.PostMachineBox;
+            foreach (PostMachineBox pmb in postmb)
+            {
+                if (pmb.FkPostMachineidPostMachine == id)
+                {
+                    if (pmb.FkPackageidPackage == null)
+                    {
+                        return pmb;
+                    }
+                }
+            }
+            return null;
         }
 
         private bool PackageExists(int id)
@@ -172,6 +199,98 @@ namespace pastomatai.Controllers
             ViewData["FkLoggedInUseridEndUser"] = new SelectList(_context.LoggedInUser, "IdEndUser", "Email", package.FkLoggedInUseridEndUser);
             ViewData["FkTerminalidTerminal"] = new SelectList(_context.Terminal, "IdTerminal", "PhoneNumber", package.FkTerminalidTerminal);
             return View(package);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PinConfirmation(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var package = await _context.Package.FindAsync(id);
+
+            if (package == null)
+            {
+                return NotFound();
+            }
+            int pmbId = getPostMachineId(package);
+            if (pmbId == -1)
+            {
+                return NotFound();
+            }
+            package.PostMachineBox = await _context.PostMachineBox.FindAsync(pmbId);
+            ViewData["FkEndUseridEndUser"] = new SelectList(_context.EndUser, "IdEndUser", "PhoneNumber", package.FkEndUseridEndUser);
+            ViewData["FkLoggedInUseridEndUser"] = new SelectList(_context.LoggedInUser, "IdEndUser", "Email", package.FkLoggedInUseridEndUser);
+            ViewData["FkTerminalidTerminal"] = new SelectList(_context.Terminal, "IdTerminal", "PhoneNumber", package.FkTerminalidTerminal);
+            ViewData["PostMachineBox"] = new SelectList(_context.PostMachineBox, "FkPackageidPackage", "Pin", package.PostMachineBox);
+
+            return View(package);
+        }
+
+        private int getPostMachineId(Package package)
+        {
+            var postmb = _context.PostMachineBox;
+            foreach (PostMachineBox pmb in postmb)
+            {
+                if (pmb.FkPackageidPackage == package.IdPackage)
+                    return pmb.IdPostMachineBox;
+            }
+            return -1;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PinConfirmation(int? packageId, string pin)
+        {
+            if (packageId == null)
+            {
+                return NotFound();
+            }
+            var package = await _context.Package.FindAsync(packageId);
+
+            int pmbId = getPostMachineId(package);
+            if (pmbId == -1)
+            {
+                return NotFound();
+            }
+            package.PostMachineBox = await _context.PostMachineBox.FindAsync(pmbId);
+            var correct = checkPin(package, pin);
+            if (correct == true)
+            {
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        if (package.PackageState.Contains("WaitsForPickup"))
+                        { package.PackageState = "Delivered"; }
+
+                        _context.Update(package);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!PackageExists(package.IdPackage))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                }
+                return RedirectToAction("Index", package);
+            }
+            else
+                // try again if pin is incorrect
+                return RedirectToAction("PinConfirmation");
+        }
+
+        private bool checkPin(Package package, string pin)
+        {
+            if (package.PostMachineBox.Pin.Contains(pin))
+                return true;
+            return false;
         }
     }
 }
